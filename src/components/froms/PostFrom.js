d9f5f3bc6dca634b85a51ef0260c746e25f1ddfe
Md. Saydur Rahman <saydur15-8853@diu.edu.bd>
2021-06-10 05:04:54 +0600

    ui

--- src/components/froms/PostFrom.js
+++ src/components/froms/PostFrom.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 
 const initialState = {
-    name:'',
-    email:'',
-    phonenumber:'',
-    bio:'',
-    gender: '',
-    birthday:'',
+    bio1:'',
+    bio2:'',
+    bio3:'',
+    bio4:'',
+    radio1: '',
+    radio2: '',
+    radio3: '',
     skills: []
 }
 class PostFrom extends React.Component{
@@ -61,105 +62,39 @@ class PostFrom extends React.Component{
                 <div className='header'>Survey Form</div>
             <hr></hr>
         <form ref={this.myForm} onSubmit= {this.submitHandler}>
-            <div className="row">
-            <div className= 'form-group col-md-6'>
-                <label htmlFor='name'> Enter Your Full name: </label>
-                <input 
-                className ='form-control'
-                type = 'text' 
-                placeholder='Enter Your Name'
-                name='name'
-                id='name'
-                value={this.state.name}
-                onChange = {this.changeHandler}
-                />
-            </div>
+           
+                {/* Question 1 */}
 
-            <div className= 'form-group col-md-6'>
-                <label htmlFor='email'> Enter Your email:</label>
-                <input
-                className ='form-control' 
-                type = 'email' 
-                placeholder='Enter Your Email'
-                name='email'
-                id='email'
-                value={this.state.email}
-                onChange = {this.changeHandler}
-                />
-            </div>
-            </div>
-            &nbsp;
-
-            <div className="row">
-            <div className= 'form-group col-md-6'>
-            <label htmlFor="birthday">Birthday:</label>
-            <input className ='form-control'
-            type="date" 
-            id="birthday" 
-            name="birthday"
-            value={this.state.birthday}
-            onChange ={this.changeHandler}/>
-            </div>
-
-
-            <div className= 'form-group col-md-6'>
-                <label htmlFor='phonenumber'> Enter Your Phone Number: </label>
-                <input 
-                className ='form-control'
-                type = 'number' 
-                placeholder='Enter Your Phone Number'
-                name='phonenumber'
-                id='phonenumber'
-                value={this.state.phonenumber}
-                onChange = {this.changeHandler}/>
-            </div>
-            </div>
-            &nbsp;
-
-            <div className= 'form-group'>
-                <label htmlFor='bio'> Enter Your Bio: </label>
-                <textarea 
-                className ='form-control'
-                type = 'text' 
-                placeholder='Enter Your Bio'
-                name='bio'
-                id='bio'
-                value={this.state.bio}
-                onChange = {this.changeHandler}/>
-            </div>
-            &nbsp;
-
-            <div className="row">
-            <div className= 'form-group col-md-4'>
-            <label htmlFor="gender"> Gender : </label>
+                <div className= 'form-group col-md-4'>
+            <label htmlFor="radio1"> Q1 : </label>
             <br></br>
             &nbsp;&nbsp;&nbsp;
                 <div className= "form-check">
-                    <input onChange={ this.changeHandler} type="radio" name="gender" id="gender1" value='Male'></input> 
+                    <input onChange={ this.changeHandler} type="radio" name="radio1" id="radio11" value='Male'></input> 
                     &nbsp;
-                    <label htmlFor = 'gender1'> Male </label>
+                    <label htmlFor = 'radio11'> Male </label>
                 </div>
                 &nbsp;&nbsp;&nbsp;&nbsp;
 
                 <div className= "form-check">
-                    <input onChange={ this.changeHandler} type="radio" name="gender" id="gender2" value='Female'></input>
+                    <input onChange={ this.changeHandler} type="radio" name="radio1" id="radio12" value='Female'></input>
                     &nbsp;
-                    <label htmlFor = 'gender2'> Female </label>
+                    <label htmlFor = 'radio12'> Female </label>
                 </div>
                 &nbsp;&nbsp;&nbsp;&nbsp;
 
                 <div className= "form-check">
-                    <input onChange={ this.changeHandler} type="radio" name="gender" id="gender3" value='Other'></input>
+                    <input onChange={ this.changeHandler} type="radio" name="radio1" id="radio13" value='Other'></input>
                     &nbsp;
-                    <label htmlFor = 'gender3'> Other </label>
+                    <label htmlFor = 'radio13'> Other </label>
                 </div>
             </div>
 
 
-            
+            {/* Question 2 */}
 
             <div className='form-skill col-md-8' > 
-            <label htmlFor="skill">Skills :</label><br/>&nbsp;
+            <label htmlFor="skill">Q2 :</label><br/>&nbsp;
                 <div className='checkboxes' style={{display:'flex', flexDirection:'row'}}>
                     <div className= 'form-check'>
                     <input onChange={this.changeHandler} className = 'form-check-input' name='skills' id='js' value='javascript' type="checkbox"/>
@@ -180,8 +115,130 @@ class PostFrom extends React.Component{
                     <label htmlFor="python"> Python</label>
                 </div>
                 </div>
+            </div> &nbsp;
+
+
+            {/* Question 3 */}
+
+            <div className= 'form-group'>
+                <label htmlFor='bio1'>Q3 : </label>
+                <textarea 
+                className ='form-control'
+                type = 'text' 
+                placeholder='Enter Your Bio1'
+                name='bio1'
+                id='bio1'
+                value={this.state.bio1}
+                onChange = {this.changeHandler}/>
             </div>
-        </div>
+            &nbsp;
+
+            {/* Question 4 */}
+
+            <div className= 'form-group'>
+                <label htmlFor='bio2'>Q4 : </label>
+                <textarea 
+                className ='form-control'
+                type = 'text' 
+                placeholder='Enter Your Bio2'
+                name='bio2'
+                id='bio2'
+                value={this.state.bio2}
+                onChange = {this.changeHandler}/>
+            </div>
+            &nbsp;
+
+
+
+                {/* Question 5 */}
+
+            <div className= 'form-group col-md-4'>
+            <label htmlFor="radio2"> Q5 : </label>
+            <br></br>
+            &nbsp;&nbsp;&nbsp;
+                <div className= "form-check">
+                    <input onChange={ this.changeHandler} type="radio" name="radio2" id="radio21" value='Male'></input> 
+                    &nbsp;
+                    <label htmlFor = 'radio21'> Male </label>
+                </div>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+
+                <div className= "form-check">
+                    <input onChange={ this.changeHandler} type="radio" name="radio2" id="radio22" value='Female'></input>
+                    &nbsp;
+                    <label htmlFor = 'radio22'> Female </label>
+                </div>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+
+                <div className= "form-check">
+                    <input onChange={ this.changeHandler} type="radio" name="radio2" id="radio23" value='Other'></input>
+                    &nbsp;
+                    <label htmlFor = 'radio23'> Other </label>
+                </div>
+            </div>
+
+            &nbsp;
+            
+
+            {/* Question 6 */}
+
+            <div className= 'form-group'>
+                <label htmlFor='bio3'>Q6 : </label>
+                <textarea 
+                className ='form-control'
+                type = 'text' 
+                placeholder='Enter Your Bio3'
+                name='bio3'
+                id='bio3'
+                value={this.state.bio3}
+                onChange = {this.changeHandler}/>
+            </div> &nbsp;
+
+            
+
+
+                     {/* Question 7 */}
+
+            <div className= 'form-group col-md-4'>
+            <label htmlFor="radio3"> Q7 : </label>
+            <br></br>
+            &nbsp;&nbsp;&nbsp;
+                <div className= "form-check">
+                    <input onChange={ this.changeHandler} type="radio" name="radio3" id="radio31" value='Male'></input> 
+                    &nbsp;
+                    <label htmlFor = 'radio31'> Male </label>
+                </div>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+
+                <div className= "form-check">
+                    <input onChange={ this.changeHandler} type="radio" name="radio3" id="radio32" value='Female'></input>
+                    &nbsp;
+                    <label htmlFor = 'radio32'> Female </label>
+                </div>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+
+                <div className= "form-check">
+                    <input onChange={ this.changeHandler} type="radio" name="radio3" id="radio3" value='Other'></input>
+                    &nbsp;
+                    <label htmlFor = 'radio3'> Other </label>
+                </div>
+            </div> &nbsp;
+
+
+                    {/* Question 8 */}
+
+            <div className= 'form-group'>
+                <label htmlFor='bio4'>Q8 : </label>
+                <textarea 
+                className ='form-control'
+                type = 'text' 
+                placeholder='Enter Your Bio4'
+                name='bio4'
+                id='bio4'
+                value={this.state.bio4}
+                onChange = {this.changeHandler}/>
+            </div>
+        
 
             <button className = 'btn btn-primary' type = 'submit'> Submit </button>
         </form>
